052f02e5de75f4fe2459bcb2e02511c1cc1afbec
unkn3wn <reyesfernando151@gmail.com>
2023-01-19 22:46:36 -0800

    api coded out not tested if works

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -1,7 +1,52 @@
-export async function allTasks(){
-    const response = await fetch(
-        "/routes/tasks"
-    )
-    const result = await response.json();
-    return result; 
+export async function allTasks() {
+  const response = await fetch("/routes/tasks");
+  const result = await response.json();
+  return result;
+}
+
+export async function singleTask(taskId) {
+  const response = await fetch(`/routes/tasks/${taskId}`);
+  const result = await response.json();
+  return result;
+}
+
+export async function createTask() {
+  const response = await fetch("/routes/tasks", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      description,
+    }),
+  });
+  const result = await response.json();
+  return result;
+}
+
+export async function updateTask(taskId, title, description) {
+  const repsonse = await fetch(`/routes/tasks/${taskId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      description,
+    }),
+  });
+  const result = await repsonse.json();
+  return result;
+}
+
+export async function deleteTask(taskId) {
+  const repsonse = await fetch(`/routes/tasks/${taskId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const result = await response.json();
+  return result;
 }
