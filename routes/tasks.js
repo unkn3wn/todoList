81f4fd1be8b7f488014f882d45fddc5414e2327e
unkn3wn <reyesfernando151@gmail.com>
2023-01-19 20:38:39 -0800

    just need to create one more route and can start the front end

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,8 +2,6 @@ const express = require("express");
 const { Tasks } = require("../db/adapters/index");
 const taskRouter = express.Router();
 
-
-
 taskRouter.get("/tasks", async (req, res, next) => {
   try {
     const allTask = await Tasks.getAllTask();
@@ -13,5 +11,53 @@ taskRouter.get("/tasks", async (req, res, next) => {
   }
 });
 
+taskRouter.get("/tasks/:taskId", async (req, res, next) => {
+  const { taskId } = req.params;
+  try {
+    const singleTask = await Tasks.getSingleTask(taskId);
+    res.send(singleTask);
+  } catch (error) {
+    next(error);
+  }
+});
+
+taskRouter.post("/tasks", async (req, res, next) => {
+  try {
+    const { title, description } = req.body;
+    const newTask = {
+      title,
+      description,
+    };
+    const theNewTask = await Tasks.createTask(newTask);
+    res.send(theNewTask);
+  } catch (error) {
+    next(error);
+  }
+});
+
+taskRouter.patch("/tasks/:taskId", async (req, res, next) => {
+  const { taskId } = req.params;
+  const { title, description } = req.body;
+  const updateFields = {};
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (description) {
+    updateFields.description = description;
+  }
+
+  try {
+    const singleTask = await Tasks.getSingleTask(taskId);
+    console.log(singleTask);
+    if(singleTask){
+      const updatedTask = await Tasks.updateTask(taskId, updateFields);
+      res.send({ task: updatedTask });
+    }
+    
+  } catch ({ title, description }) {
+    next({ title, description });
+  }
+});
 
 module.exports = taskRouter;
